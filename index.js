9c76aba49cf14f494832c8d3e05f1400b1d05896
Chase <chaseit12@gmail.com>
2018-04-01 20:39:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers){
+  let newarr = drivers.map((each_driver)=>{
+    each_driver = each_driver.toLowerCase()
+    return each_driver
+  })
+  return newarr
+}
+
+function nameToAttributes(drivers_arr){
+  let results_arr = drivers_arr.map((each_name_string)=>{
+    let name_arr = each_name_string.split(' ')
+    return Object.assign({}, {firstName:name_arr[0]}, {lastName:name_arr[1]})
+  })
+  return results_arr
+}
+
+function attributesToPhrase(){
+  
+}
